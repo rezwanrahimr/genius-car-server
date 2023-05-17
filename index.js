bae8f1494b136e1dc7867c4a6b12529239a1b381
rezwanrahimr <rezwanrahim69@gmail.com>
2023-05-18 00:09:35 +0600

    added jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.port || 5000;
 require("dotenv").config();
@@ -21,12 +22,38 @@ const client = new MongoClient(uri, {
   },
 });
 
+//
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.status(401).send({ message: "unAuthorize" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      res.status(401).send({ message: "unAuthorize" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const serviceCollection = client.db("genius-car").collection("services");
     const orderCollection = client.db("genius-car").collection("order");
+
+    // jwt
+    app.post("/jwt", (req, res) => {
+      const email = req.body;
+      console.log(email);
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
 
     // Get Services
     app.get("/services", async (req, res) => {
@@ -53,17 +80,22 @@ async function run() {
     });
 
     // Get Order
-    app.get("/orders", async (req, res) => {
-      console.log(req.query);
-      let quarry = {};
-      if (req.query?.email) {
-        quarry = {
-          email: req.query?.email,
-        };
+    app.get("/orders", verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+      if (decoded?.email === req.query?.email) {
+        let quarry = {};
+        if (req.query?.email) {
+          quarry = {
+            email: req.query?.email,
+          };
+        }
+        const cursor = orderCollection.find(quarry);
+        const result = await cursor.toArray();
+        res.send(result);
+      } else {
+        console.log("success");
+        return res.status(403).send({ message: "unAuthorize" });
       }
-      const cursor = orderCollection.find(quarry);
-      const result = await cursor.toArray();
-      res.send(result);
     });
 
     // Delete Order
